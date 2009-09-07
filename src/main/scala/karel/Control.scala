30e94ab2951f9c8825834bb05df4ad3a7ab42e2a
Dave Copeland <dave.copeland@positiveenergyusa.com>
2009-09-07 19:25:51 -0400

    better documentation

--- src/main/scala/karel/Control.scala
+++ src/main/scala/karel/Control.scala
@@ -2,17 +2,33 @@ package karel
 
 import karel.internal._;
 
+/** Control Structures */
 object Control {
   var subs:Map[Symbol,BLOCK] = Map()
 
   implicit def instructionToBlock(i:Instruction) = BLOCK(i)
 
+  /** 
+  * Define a subroutine
+  * @param name the name of the subroutine
+  * @param b a block defining the subroutine
+  */
   def DEF(name:Symbol,b:BLOCK) = subs = subs + (name -> b)
 
+  /** 
+   * Call a subroutine
+   * @param name the name of the subroutine to call
+   */
   case class CALL(name:Symbol) extends Instruction {
-    def apply(w:World,k:Karel) = subs.get(name).getOrElse(null)(w,k)
+    def apply(w:World,k:Karel) = subs.get(name) match {
+      case Some(b:BLOCK) => b(w,k)
+      case None => throw new Explosion
+    }
   }
 
+  /** 
+    * Define a block of instructions
+    */
   case class BLOCK(s:Instruction*) extends KarelStatement[World] {
     def apply(w:World,k:Karel) = {
       var world = w
@@ -21,16 +37,18 @@ object Control {
     }
   }
 
-  case class IF(c:Condition,t:BLOCK) extends Instruction {
-    def apply(w:World,k:Karel) = {
-      if (c(w,k))
-        t(w,k)
-      else
-        w
-    }
+  /** A no-op */
+  object NOOP extends Instruction {
+    def apply(w:World,k:Karel) = w
   }
 
-  case class IF_THEN(c:Condition,t:BLOCK,e:BLOCK) extends Instruction {
+  /**
+    * A conditional with an else 
+    * @param c the condition
+    * @param t the block to execute if c is true
+    * @param e the block to execute if c is not true
+    */
+  case class IF_THEN_ELSE(c:Condition,t:BLOCK,e:BLOCK) extends Instruction {
     def apply(w:World,k:Karel) = {
       if (c(w,k))
         t(w,k)
@@ -39,6 +57,18 @@ object Control {
     }
   }
 
+  /**
+    * a conditional.
+    * @param c a Conditiont o evaluate
+    * @param t the block to evaluate if condition is true
+    */
+  case class IF(override val c:Condition,override val t:BLOCK) extends IF_THEN_ELSE(c,t,NOOP)
+
+  /**
+    * A loop
+    * @param c the condition to evaluate each time through the loop
+    * @param d the block to execute each time through the loop
+    */
   case class WHILE_DO(c:Condition,d:BLOCK) extends Instruction {
     def apply(w:World,k:Karel) = {
       var world = w
@@ -49,6 +79,11 @@ object Control {
     }
   }
 
+  /**
+    * an iterative loop
+    * @param times the number of times to iterate
+    * @param d the block to iterate each time through the loop
+    */
   case class ITERATE(times:Int,d:BLOCK) extends Instruction {
     def apply(w:World,k:Karel) = {
       var world = w
@@ -58,5 +93,4 @@ object Control {
       world
     }
   }
-
 }
