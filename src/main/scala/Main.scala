520780f276998245a1912e5fa816a0d7c48587c4
Dave Copeland <dave.copeland@positiveenergyusa.com>
2009-09-08 09:52:01 -0400

    basic example runs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,53 +1,42 @@
 import karel._
 
 object Main extends Application with KarelTheRobot {
-  var world = new World(10,10,Map(
-    (4,5) -> new Wall,
-    (4,6) -> new Wall,
-    (4,7) -> new Wall,
-    (4,8) -> new Wall,
-    (5,5) -> new Wall,
-    (6,5) -> new Wall,
-    (7,5) -> new Wall,
-    (7,4) -> new Wall,
-    (7,3) -> new Wall,
-    (3,8) -> new Beeper,
-    (0,0) -> new Beeper
-  ))
+  run(10,10, Map(
+      (4,5) -> new Wall,
+      (4,6) -> new Wall,
+      (4,7) -> new Wall,
+      (4,8) -> new Wall,
+      (5,5) -> new Wall,
+      (6,5) -> new Wall,
+      (7,5) -> new Wall,
+      (7,4) -> new Wall,
+      (7,3) -> new Wall,
+      (3,8) -> new Beeper,
+      (0,0) -> new Beeper
+    ), 
+    {
+      DEF('TURN_RIGHT,BLOCK(TURN_LEFT,TURN_LEFT,TURN_LEFT))
+      DEF('TURN_AROUND,BLOCK(TURN_LEFT,TURN_LEFT))
+      DEF('THROW_BEEPER,BLOCK(
+        IF(NEXT_TO_BEEPER,BLOCK(
+          PICK_BEEPER,
+          ITERATE(5,BLOCK(IF(FRONT_IS_CLEAR,MOVE))),
+          PUT_BEEPER,
+          CALL('TURN_AROUND),
+          ITERATE(5,BLOCK(IF(FRONT_IS_CLEAR,MOVE))),
+          CALL('TURN_AROUND)
+        ))
+      ))
 
-
-  val k = new Karel(North)
-  world = world + ((k,(1,2)))
-
-  DEF('TURN_RIGHT,BLOCK(TURN_LEFT,TURN_LEFT,TURN_LEFT))
-  DEF('TURN_AROUND,BLOCK(TURN_LEFT,TURN_LEFT))
-  DEF('THROW_BEEPER,BLOCK(
-    IF(NEXT_TO_BEEPER,BLOCK(
-      PICK_BEEPER,
-      ITERATE(5,BLOCK(IF(FRONT_IS_CLEAR,MOVE))),
-      PUT_BEEPER,
-      CALL('TURN_AROUND),
-      ITERATE(5,BLOCK(IF(FRONT_IS_CLEAR,MOVE))),
-      CALL('TURN_AROUND)
+      DEF ('RUN,BLOCK(
+        WHILE_DO(FRONT_IS_CLEAR,MOVE),
+        CALL('TURN_AROUND)
+      ))
+    },
+    BLOCK(
+      WHILE_DO(NOT_FACING_EAST,TURN_LEFT),
+      CALL('THROW_BEEPER),
+      CALL('TURN_RIGHT),
+      CALL('RUN)
     ))
-  ))
-
-  DEF ('RUN,BLOCK(
-    WHILE_DO(FRONT_IS_CLEAR,MOVE),
-    CALL('TURN_AROUND)
-  ))
-
-  println(world)
-
-  world = BLOCK(
-    MOVE,
-    MOVE,
-    TURN_LEFT,
-    MOVE,
-    CALL('TURN_AROUND),
-    CALL('THROW_BEEPER),
-    CALL('RUN)
-  )(world,k)
-
-  println(world)
 }
--- src/main/scala/karel/Karel.scala
+++ src/main/scala/karel/Karel.scala
@@ -4,7 +4,7 @@ trait KarelTheRobot extends Conditions with Controls with Instructions {
 
   def run(l:Int,w:Int,map:Map[(Int,Int),Element],defs: => Unit,code: BLOCK) = {
     val k = new Karel(North)
-    val world = new World(l,w,map) + (k,5,5)
+    val world = new World(l,w,map) + (k,0,0)
     defs
     println(world)
     println(code(world,k))
